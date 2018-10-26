f5d78f8511bf49834a7fd2516177fcce623e7869
ddkatona <ddkatona@gmail.com>
2018-10-26 17:55:45 +0200

    single image lookup

--- lookup.py
+++ lookup.py
@@ -1,18 +1,21 @@
 import numpy as np
+import vis
 
-
-# lables in latents_values: (0'color', 1'shape', 2'scale', 3'orientation', 4'posX', 5'posY')
+# lables in latents_values idx: ('color':0, 'shape':1, 'scale':2, 'orientation':3, 'posX':4, 'posY':5)
+# lables in latents_values qnty: ('color':1, 'shape':3, 'scale':6, 'orientation':40, 'posX':32, 'posY':32)
+# color is not a parameter, because it's always '1'
 def get_image(shape, scale, orientation, posX, posY):
     data = np.load("datasets/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz")
-    labels = data['latents_values']
-    print(labels[00])
-    print(labels[1])
+    imgs = data['imgs']
 
-    c = 0
-    for i in range(len(labels)):
-        if labels[i][1] == shape and labels[i][2] == scale:
-            #print(i)
-            c = c + 1
-    print(c)
+    posY_const = 1
+    posX_const = 32 * posY_const
+    orientation_const = 32 * posX_const
+    scale_const = 40 * orientation_const
+    shape_const = 6 * scale_const
 
-get_image(2, 0.5, 0, 0, 0)
+    index = shape * shape_const + scale * scale_const + orientation * orientation_const + posX * posX_const + posY * posY_const
+    return np.array([ imgs[index] ])
+
+#image = get_image(2, 5, 39, 31, 31)
+#vis.plotImages(np.expand_dims(image, axis=3), len(image), 1, 'pictures/lookup/test')
