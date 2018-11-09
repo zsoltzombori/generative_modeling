d47f28d98445935e41abd022fc7bda6babde5881
ddkatona <ddkatona@gmail.com>
2018-11-09 03:24:05 +0100

    grid evaluation by lines

--- lookup.py
+++ lookup.py
@@ -26,6 +26,18 @@ def find_indices_shift(shape, scale, orientation):
             indices.append(find_index(shape, scale, orientation, posX, posY))
     return np.array(indices)
 
+def find_indices_lineY(shape, scale, orientation, posY):
+    indices = []
+    for posX in range(32):
+            indices.append(find_index(shape, scale, orientation, posX, posY))
+    return np.array(indices)
+
+def find_indices_lineX(shape, scale, orientation, posX):
+    indices = []
+    for posY in range(32):
+            indices.append(find_index(shape, scale, orientation, posX, posY))
+    return np.array(indices)
+
 def get_images(indices):
     data = np.load("datasets/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz")
     imgs = data['imgs']
@@ -34,23 +46,73 @@ def get_images(indices):
 modelDict = model_IO.load_autoencoder(args)
 encoder = modelDict.encoder
 
-for o in range(25, 30):
-    for s in range(0,2):
 
-        idc = find_indices_shift(2, s, o)
-        images = get_images(idc)
-        #vis.plotImages(np.expand_dims(images, axis=3), 32, 32, 'pictures/lookup/test
+idc = find_indices_shift(0,0,0)
+images = get_images(idc)
+#vis.plotImages(np.expand_dims(images, axis=3), 32, 32, 'pictures/lookup/test
 
-        batch_size = args.batch_size
-        images = np.expand_dims(images, axis=3)
-        images = images[:(len(images) // args.batch_size * args.batch_size)]
-        res = encoder.predict(images, batch_size = batch_size)
-        pca = PCA(n_components=2)
-        points = pca.fit_transform(res[1])
-        x, y = points.T
-        colors = [ [(i%32)/32, (i-i%32)/1024, 0] for i in range(1000)]
-        name = "pictures/mass/plot_scale" + str(s) + "ori" + str(o)
-        plt.scatter(x, y, c=colors)
-        plt.savefig(name)
-        print(name + ".png is created!")
-        plt.close()
+batch_size = args.batch_size
+images = np.expand_dims(images, axis=3)
+images = images[:(len(images) // args.batch_size * args.batch_size)]
+res = encoder.predict(images, batch_size = batch_size)
+pca = PCA(n_components=2)
+points = pca.fit_transform(res[1])
+
+sumY = 0
+sumX = 0
+
+for X in range(32):
+    rng = range(X, 31*32 + X, 32)
+    #print(rng)
+    pointsX = points[rng]
+    x, y = pointsX.T
+    #colors = [ [(i%32)/32, (i-i%32)/1024, 0] for i in range(1000)]
+    colors = [ [ 0, 0, i/30] for i in range(31)]
+    name = "pictures/graphs/vae_conv/X/lineX" + str(X)
+    plt.scatter(x, y, c=colors)
+    plt.savefig(name)
+    plt.close()
+    #print(name + ".png is created!")
+    pca2 = PCA(n_components=1)
+    line = pca2.fit(res[1])
+    sumX = sumX + pca2.explained_variance_ratio_[0]
+
+print("X: " + str(sumX/31))
+
+for Y in range(31):
+    rng = range(Y*32, Y*32+32)
+    #print(rng)
+    pointsY = points[rng]
+    x, y = pointsY.T
+    #colors = [ [(i%32)/32, (i-i%32)/1024, 0] for i in range(1000)]
+    colors = [ [ i/31, 0, 0] for i in range(32)]
+    name = "pictures/graphs/vae_conv/Y/lineY" + str(Y)
+    plt.scatter(x, y, c=colors)
+    plt.savefig(name)
+    plt.close()
+    #print(name + ".png is created!")
+    pca2 = PCA(n_components=1)
+    line = pca2.fit(res[1])
+    sumY = sumY + pca2.explained_variance_ratio_[0]
+
+print("Y: " + str(sumY/30))
+
+def two_lines(X, Y):
+    #compare
+    #X0
+    pointsX0 = points[ range(X, 31*32 + X, 32) ]
+    colorsX = [ [ 0, 0, 1] for i in range(0, 31)]
+    #Y0
+    pointsY0 = points[ range(Y*32, Y*32+32) ]
+    colorsY = [ [ 1, 0, 0] for i in range(0, 32)]
+
+    colors = np.append(colorsX, colorsY, axis=0)
+    pointsXY0 = np.append(pointsX0, pointsY0, axis=0)
+    x, y = pointsXY0.T
+
+    name = "pictures/graphs/vae_conv/XY/line" + str(X) + "X" + str(Y) + "Y"
+    plt.scatter(x, y, c=colors)
+    plt.savefig(name)
+    plt.close()
+
+#two_lines(0,0)
