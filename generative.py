a9053fba60767fe2526d79cc1f07ff4f8e812489
doma <doma945@gmail.com>
2018-11-29 16:12:52 +0100

    Fixed bug: generative.py

--- generative.py
+++ generative.py
@@ -7,7 +7,6 @@ import data
 
 import autoencoder
 import gan
-import wgan_gp
 
 # load parameters
 args = params.getArgs()
@@ -33,9 +32,7 @@ args.original_size = np.prod(args.original_shape)
 
 if args.model_type == "autoencoder":
     autoencoder.run(args, (x_train, x_test))
-elif args.model_type in ("wgan-gp"):
-    gan.run(args, (x_train, x_test))
-elif args.model_type in ("gan", "wgan"):
+elif args.model_type in ("gan", "wgan","wgan-gp"):
     gan.run(args, (x_train, x_test))
 else:
     assert False, "Unrecognized model_type: {}".format(args.model_type)
