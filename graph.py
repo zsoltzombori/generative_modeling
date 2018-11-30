ece31dcb0b07cb79c1eecb3087bf9cf994baf1da
ddkatona <ddkatona@gmail.com>
2018-11-30 14:07:38 +0100

    graph

---
+++ graph.py
@@ -0,0 +1,30 @@
+import os
+from matplotlib import pyplot as plt
+import numpy as np
+
+def get_losses(dir):
+    with open("pictures/" + dir + "/main.log","r") as file:
+        data=file.read()
+        lines=data.split("\n")
+        ls=lines[-11].split(" ")
+        print(lines)
+        losses=[ls[5],ls[8],ls[11],ls[14],ls[17],ls[20],ls[23],ls[26]]
+        return losses
+
+def plot_losses(name):
+    dirs = [dI for dI in os.listdir("pictures") if os.path.isdir(os.path.join('pictures',dI)) and name in dI]
+    qnt = len(dirs)
+
+    x=[]
+    y=[]
+    for dir in dirs:
+        ds=dir.split("_")
+        param=ds[-1]
+        x.append(float(param))
+        y.append(float(get_losses(dir)[0]))
+
+    plt.scatter(np.array(x), np.array(y))
+    plt.savefig(name + "-loss")
+    plt.close()
+
+plot_losses("vae_ldim")
